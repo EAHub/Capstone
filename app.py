68e6ffec886fc661f1d66d97a402e96ee173c4a1
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-27 14:31:24 -0400

    no pg2

--- app.py
+++ app.py
@@ -23,14 +23,13 @@ app = Flask(__name__)
 def main():
   return redirect('/Page1')
 
-@app.route('/Page1', methods=['GET','POST'])
+@app.route('/Page1')
 def Page1():
   return render_template('Page1.html')
 
-
-@app.route('/Page2', methods=['GET', 'POST'])
-def Page2():
-	return render_template('Page2.html')
+#@app.route('/Page2')
+#def Page2():
+#	return render_template('Page2.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
