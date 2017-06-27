57813ee5cc5392603f81d200dd643d1da1e7b6f6
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-27 14:41:57 -0400

    pg2 back in

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def main():
 def Page1():
   return render_template('Page1.html')
 
-#@app.route('/Page2')
-#def Page2():
-#	return render_template('Page2.html')
+@app.route('/Page2')
+def Page2():
+	return render_template('Page2.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
